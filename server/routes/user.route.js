bfb5e498742c4844810158c531d3f5383ae9bda0
Akash <akashks838@gmail.com>
2025-01-04 23:24:27 +0530

    Recieved request API

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,6 +2,9 @@ const express = require("express");
 const userRouter = express.Router();
 const User = require("../model/user.model");
 const userAuth = require("../middleware/auth");
+const connectionRequest = require("../model/connection.model");
+
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
 // GET ALL USERS
 userRouter.get("/feed", userAuth, async (req, res) => {
@@ -10,6 +13,27 @@ userRouter.get("/feed", userAuth, async (req, res) => {
     res.status(200).send(users);
   } catch (error) {
     res.status(400).send("Can not get users" + error.message);
+  }
+});
+
+// All RECIEVED REQUESTS
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await connectionRequest
+      .find({
+        toUserId: loggedInUser._id,
+        status: "interested",
+      })
+      .populate("fromUserId", USER_SAFE_DATA);
+
+    res.json({
+      message: "Data fetched successfully",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    req.statusCode(400).send("ERROR: " + err.message);
   }
 });
 
